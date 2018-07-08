aa4f07f652f9390a10064bd81318ae1ac7c5240c
KID <jyhsu2000@gmail.com>
2018-07-08 11:33:23 +0800

    Fix typo

--- src/Config/auth-management.php
+++ src/Config/auth-management.php
@@ -12,12 +12,12 @@ return [
     'single-login'         => env('AUTH_MANAGEMENT_SINGLE_LOGIN', false),
 
     /*
-     * 是否紀錄用戶最後活動時間
+     * 是否記錄用戶最後活動時間
      */
     'record-last-activity' => env('AUTH_MANAGEMENT_RECORD_LAST', false),
 
     /*
-     * 是否紀錄用戶登入/登出事件
+     * 是否記錄用戶登入/登出事件
      */
     'logging'              => env('AUTH_MANAGEMENT_LOGGING', false),
 ];
